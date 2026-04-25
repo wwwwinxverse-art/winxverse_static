10fff7b280615fb9670abb86841417cd60afd8da
SubburamK <subburam@e2o.in>
2026-04-25 13:42:53 +0530

    complete the static

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,7 @@
 import AboutContent from "../components/AboutUs/AboutContent";
 import AboutFounder from "../components/AboutUs/AboutFounder";
 import AboutHeader from "../components/AboutUs/AboutHeader";
-import OurTeam from "../components/AboutUs/OurTeam";
+// import OurTeam from "../components/AboutUs/OurTeam";
 import WhyChooseUs from "../components/AboutUs/WhyChooseUs";
 
 const About = () => {
